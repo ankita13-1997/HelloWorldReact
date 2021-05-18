4896fd6237ad981cdc7fb4b18095307987d18dec
ankita13-1997 <parhiankita@gmail.com>
2021-05-18 12:18:05 +0530

    [initialized]ankita : created hello world react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from 'react';
+import logo2 from './assets/logo1.jpg';
 
+class App extends React.Component{
+
+  constructor(){
+   super();
+   this.state = {
+    title : "Hello From Ankita"
+  }
+  }
+  
+  render() {
+    return (
+      <div>
+        <img src={logo2} alt="my logo"></img>
+        <h1> {this.state.title} </h1>
+      </div>
+    );
+  }
+    
+}
+ 
 export default App;
